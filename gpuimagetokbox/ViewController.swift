7848a791f6bbeb13a168292377945193d10c3a33
Fujiki Takeshi <Takeshi.Fujiki@jp.sony.com>
2015-12-16 19:04:14 +0900

    Initial Commit

---
+++ gpuimagetokbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gpuimagetokbox
+//
+//  Created by Fujiki Takeshi on 12/16/15.
+//  Copyright © 2015 takecian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
